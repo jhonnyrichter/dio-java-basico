c00b125f572d5f1cd2d6f294ff30413048a0f9b0
Jhonny Cesar Richter <jhonnyjcrr@gmail.com>
2024-05-02 09:47:32 -0300

    criando o projeto para exemplos tipos variaveis

--- README.md
+++
@@ -1,3 +0,0 @@
-# dio-java-basico
-Repositório para armazenar todo o conteúdo do curso de Java Básico
-Alterando conteúdo de um arquivo forma local #Teste
--- TIPOS DE VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS DE VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("OLÁ PESSOAL!");
+        System.out.println("Hello, World!");
     }
 }
